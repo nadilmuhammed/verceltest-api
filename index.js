2c984ba7012a8d541d77e457c0cc976e72344793
nadil11 <nadilnidhal111@gmail.com>
2024-07-28 14:14:36 +0530

    cors added

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ connect();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: ["https://verceltest-client.vercel.app"],
+  methods: [ "GET", "POST", "PUT", "DELETE" ],
+  credentials: true
+}));
 
 app.use("/api/auth", authRoute);
 
